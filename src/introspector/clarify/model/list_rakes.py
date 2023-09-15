3ba2cf9905cfa71692d44252c04069aabeb4f013
mike dupont <mike.dupont@introspector.local>
2023-09-15 02:55:49 -0400

    rake list

--- src/introspector/clarify/model/list_rakes.py
+++ src/introspector/clarify/model/list_rakes.py
@@ -11,5 +11,7 @@ for a in ap:
     wf = a .list_workflows()
     #print(a,.sjoinwf)
     for w in wf:
-        print({"a":str(a),
-                          "w":str(w)})
+        #print({"a":str(a),
+        #"w":str(w)})
+        print(w.id)
+        #print(json.dumps(w2))
